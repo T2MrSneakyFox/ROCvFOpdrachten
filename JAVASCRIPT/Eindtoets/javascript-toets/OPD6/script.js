af151031813786f66cbd53b5bccecc7275b0e9c6
MrSneakyFox <Pepijn222Real@gmail.com>
2022-03-30 10:17:17 +0200

    V0.1.9
    Part 1/2 JAVASCRIPT

---
+++ JAVASCRIPT/Eindtoets/javascript-toets/OPD6/script.js
@@ -0,0 +1,32 @@
+Counter = 0;
+CounterColor = 0;
+
+document.addEventListener("click", function (event) {
+  if (event.target.id == "buttonone" && Counter <= 48) {
+    Counter++;
+    CounterColor++;
+    var div = document.createElement("div");
+    div.style.width = "169px";
+    div.style.height = "100px";
+    div.innerHTML = Counter;
+
+    if (CounterColor == 1) {
+      div.style.background = "blue";
+    }
+    if (CounterColor == 2) {
+      div.style.background = "green";
+    }
+    if (CounterColor == 3) {
+      div.style.background = "yellow";
+    }
+    if (CounterColor == 4) {
+      div.style.background = "purple";
+    }
+    if (CounterColor == 5) {
+      div.style.background = "red";
+      CounterColor = 0;
+    }
+
+    document.getElementsByClassName("container")[0].appendChild(div);
+  }
+});
